990df5ecdb4dff2748b50c057633e71908c116b4
swan-london <swapniluk.3010@gmail.com>
2023-08-09 21:54:44 +0100

    added label component

---
+++ src/stories/Label.stories.js
@@ -0,0 +1,14 @@
+import { Label } from './Label';
+
+export default {
+  title: 'Example/Label',
+  component: Label,
+  tags: ['autodocs'],
+}
+
+export const Standard = {
+  args: {
+    text: 'This is a label',
+    classes: 'cmp-label-regular',
+  },
+}
